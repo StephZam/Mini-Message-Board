50fb44a535c23fa3f038741ebad9fe89e091309e
StephZam <stephysam008@gmail.com>
2025-05-23 11:25:08 +0530

    completed

--- index.js
+++ index.js
@@ -22,4 +22,24 @@ router.get('/new', (req, res) => {
     res.render('form', { title: 'New message' });
 });
 
+router.post('/new', (req, res) => {
+    const messageUser = req.body.messageUser;
+    const messageText = req.body.messageText;
+    messages.push({
+        text: messageText,
+        user: messageUser,
+        added: new Date()
+    });
+    res.redirect('/');
+})
+
+router.get('/message/:id', (req, res) => {
+    const message = messages[req.params.id];
+    if(message) {
+        res.render('message', {title: "Message Detail", message});
+    } else {
+        res.status(404).render('error', {title: "Error", message: "Message not found"});
+    }
+});
+
 module.exports = router;
